2c1c8bfa485a62d66e76d187a93b0036da370b04
Eliza Ann Benoy <Eliza_Ann_Benoy-EAB@ulster.ac.uk>
2024-10-21 18:44:26 +0100

    final

--- Week2practical4/Program.cs
+++ Week2practical4/Program.cs
@@ -1,2 +1,34 @@
-﻿Main();
+﻿using System.Collections.Specialized;
+using System.Globalization;
+
+Main();
+
+static void Main()
+{
+    Console.WriteLine("Enter a string:");
+    string str = Console.ReadLine();
+
+    Console.WriteLine("Enter no of rotation");
+    int noOfrotation= Convert.ToInt32(Console.ReadLine());
+
+    string decrypt = Decrypt( str,  noOfrotation);
+
+    Console.WriteLine($"The sentence is,{str}");
+    Console.WriteLine($"The decrypted sentence is , {decrypt}");
+
+
+}
+static string Decrypt( string line, int Rotation)
+{
+    string decdedText = " ";
+
+    foreach( char c in line)
+    {
+        int decoded = c + Rotation;
+        decdedText += (char)decoded;
+
+    }
+    return decdedText;
+}
+
 
